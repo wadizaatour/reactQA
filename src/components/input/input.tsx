9641607febef5542c36ede0fa87e6531c0e7f637
wadizaatour <zaatourwadi1@gmail.com>
2023-11-26 17:36:21 +0100

    refactor input component

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,6 +1,4 @@
 import { ChangeEvent } from "react";
-import { useDispatch } from "react-redux";
-import { setValue } from "../../redux/inputSlice";
 interface InputProps {
   type: "text" | "password" | "email"; // specify allowed input types
   label: string;
@@ -13,26 +11,25 @@ interface InputProps {
 const Input = ({
   type,
   label,
-  value,
   placeholder,
   onChange,
+  value,
   disabled = false,
 }: InputProps) => {
-  const dispatch = useDispatch();
-  
+  const lowercaseLabel = label.toLowerCase();
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value;
     onChange(newValue);
-    dispatch(setValue(newValue));
   };
 
   return (
     <div className="input-wrapper">
       <label htmlFor={label}>{label}</label>
       <input
-        aria-labelledby={label.toLowerCase()}
+        aria-labelledby={lowercaseLabel}
         type={type}
-        id={label.toLowerCase()} // Use lowercase label as ID
+        id={lowercaseLabel} // Use lowercase label as ID
         value={value}
         placeholder={placeholder}
         onChange={handleChange}
