34c1bfb5373a71342afec90721c139f049f425ae
wadizaatour <zaatourwadi1@gmail.com>
2023-11-26 15:54:49 +0100

    Improve accessibility

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,12 +1,22 @@
 interface ButtonProps {
+  ariaLabel: string;
   children: string;
   color: string;
   onClick: () => void;
 }
-const Button = ({ children, color, onClick }: ButtonProps) => {
+
+const Button = ({ ariaLabel, children, color, onClick }: ButtonProps) => {
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLButtonElement>) => {
+    if (e.key === "Enter" || e.key === " ") {
+      onClick();
+    }
+  };
   return (
     <button
+      role="button"
+      aria-label={ariaLabel}
       onClick={onClick}
+      onKeyDown={handleKeyDown}
       style={{
         backgroundColor: color,
       }}
