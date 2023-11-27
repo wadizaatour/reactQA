3824f7fcf669f78ef9256d0eec62372a8a6bd3ef
wadizaatour <zaatourwadi1@gmail.com>
2023-11-27 03:03:15 +0100

    add tests for buttn component

--- src/components/button/Button.test.tsx
+++ src/components/button/Button.test.tsx
@@ -1,7 +1,9 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Button from './Button'
 
 describe('button', () => {
+  const onClickMock = jest.fn()
+
   it('button is able to render when page is loaded', () => {
     render(
       <Button
@@ -14,5 +16,83 @@ describe('button', () => {
       </Button>
     )
     expect(screen.getByTestId('button1')).toBeInTheDocument()
+  })
+  it('calls the onClick function when clicked', () => {
+    render(
+      <Button
+        ariaLabel="Test Button"
+        testId="test-button"
+        color="blue"
+        type="button"
+        onClick={onClickMock}
+      >
+        Click me
+      </Button>
+    )
+
+    const buttonElement = screen.getByTestId('test-button')
+    fireEvent.click(buttonElement)
+
+    // Check if the onClick function is called
+    expect(onClickMock).toHaveBeenCalled()
+  })
+  it('calls the onClick function when clicked', () => {
+    render(
+      <Button
+        ariaLabel="Test Button"
+        testId="test-button"
+        color="blue"
+        type="button"
+        onClick={onClickMock}
+      >
+        Click me
+      </Button>
+    )
+
+    const buttonElement = screen.getByTestId('test-button')
+    fireEvent.click(buttonElement)
+
+    // Check if the onClick function is called
+    expect(onClickMock).toHaveBeenCalled()
+  })
+
+  it('calls the onClick function when "Enter" key is pressed', () => {
+    render(
+      <Button
+        ariaLabel="Test Button"
+        testId="test-button"
+        color="blue"
+        type="button"
+        onClick={onClickMock}
+      >
+        Click me
+      </Button>
+    )
+
+    const buttonElement = screen.getByTestId('test-button')
+    fireEvent.keyDown(buttonElement, { key: 'Enter', code: 'Enter' })
+
+    // Check if the onClick function is called
+    expect(onClickMock).toHaveBeenCalled()
+  })
+
+  it('calls the onClick function when "Space" key is pressed', () => {
+    render(
+      <Button
+        ariaLabel="Test Button"
+        testId="test-button"
+        color="blue"
+        type="button"
+        onClick={onClickMock}
+      >
+        Click me
+      </Button>
+    )
+
+    const buttonElement = screen.getByTestId('test-button')
+    fireEvent.keyDown(buttonElement, { key: ' ', code: 'Space' })
+
+    // Check if the onClick function is called
+    expect(onClickMock).toHaveBeenCalled()
   })
 })
