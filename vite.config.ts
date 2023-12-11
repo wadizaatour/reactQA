43a2960382f1a257f60b54cfaff4087cc07e10f0
wadizaatour <zaatourwadi1@gmail.com>
2023-12-11 14:40:27 +0100

    automated dev run

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 export default defineConfig({
+  server: { open: true },
   base: process.env.NODE_ENV === 'production' ? '/dist' : '',
   css: {
     modules: {
